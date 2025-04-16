1c6ccc264d14d67fff6264a1949a76c3669f8112
Bethwel-Pixel <bethwelkiptoo73@gmail.com>
2025-04-16 15:26:27 +0300

    addition and substraction

---
+++ python-challenge/python-first-challenge.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+
+'''creating a basic calculatore'''
+
+first_charactor= input('input the first number: ')
+second_character = input('second number: ')
+
+print(f"first_character + second_character= {int(first_charactor) + int(second_character)}")
+print(f"first_character / second_character={int(first_charactor) // int( second_character)}")
+print(f" first_character * second_character={int(first_charactor) *  int(second_character)}")
+print(f" first_character - second_character={int(first_charactor) -  int(second_character)}")
